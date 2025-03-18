3e57c2cb5e7744ea9fc7f79dbca372c5d1e5e128
czm <czm>
2025-03-18 13:50:02 +0800

    更改拼写错误

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,12 +48,11 @@
             // 
             // txtTableName
             // 
-            this.txtTableName.Location = new System.Drawing.Point(123, 100);
-            this.txtTableName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTableName.Location = new System.Drawing.Point(184, 150);
             this.txtTableName.Name = "txtTableName";
             this.txtTableName.Properties.Appearance.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txtTableName.Properties.Appearance.Options.UseFont = true;
-            this.txtTableName.Size = new System.Drawing.Size(199, 22);
+            this.txtTableName.Size = new System.Drawing.Size(298, 30);
             this.txtTableName.TabIndex = 0;
             // 
             // panel1
@@ -67,20 +66,18 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.txtTableName);
             this.panel1.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.panel1.Location = new System.Drawing.Point(8, 8);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(360, 508);
+            this.panel1.Size = new System.Drawing.Size(540, 762);
             this.panel1.TabIndex = 1;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button2.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(89, 380);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(134, 570);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 53);
+            this.button2.Size = new System.Drawing.Size(252, 80);
             this.button2.TabIndex = 11;
             this.button2.Text = "OutFile";
             this.button2.UseVisualStyleBackColor = false;
@@ -90,54 +87,49 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label5.Location = new System.Drawing.Point(35, 51);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(52, 76);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(87, 16);
+            this.label5.Size = new System.Drawing.Size(130, 24);
             this.label5.TabIndex = 10;
             this.label5.Text = "ClassName:";
             // 
             // txtClassName
             // 
-            this.txtClassName.Location = new System.Drawing.Point(123, 49);
-            this.txtClassName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtClassName.Location = new System.Drawing.Point(184, 74);
             this.txtClassName.Name = "txtClassName";
             this.txtClassName.Properties.Appearance.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txtClassName.Properties.Appearance.Options.UseFont = true;
-            this.txtClassName.Size = new System.Drawing.Size(199, 22);
+            this.txtClassName.Size = new System.Drawing.Size(298, 30);
             this.txtClassName.TabIndex = 9;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(89, 299);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(134, 448);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 51);
+            this.button1.Size = new System.Drawing.Size(252, 76);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Beagin";
+            this.button1.Text = "Begin";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtConn
             // 
-            this.txtConn.Location = new System.Drawing.Point(38, 245);
-            this.txtConn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtConn.Location = new System.Drawing.Point(57, 368);
             this.txtConn.Name = "txtConn";
             this.txtConn.Properties.Appearance.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txtConn.Properties.Appearance.Options.UseFont = true;
-            this.txtConn.Size = new System.Drawing.Size(285, 22);
+            this.txtConn.Size = new System.Drawing.Size(428, 30);
             this.txtConn.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(35, 217);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(52, 326);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(143, 16);
+            this.label2.Size = new System.Drawing.Size(214, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "StringConnection:";
             // 
@@ -145,10 +137,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(35, 102);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(52, 153);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.Size = new System.Drawing.Size(130, 24);
             this.label1.TabIndex = 3;
             this.label1.Text = "TableName:";
             // 
@@ -156,31 +147,28 @@
             // 
             this.panel2.Controls.Add(this.txtResult);
             this.panel2.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.panel2.Location = new System.Drawing.Point(372, 8);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Location = new System.Drawing.Point(558, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(981, 508);
+            this.panel2.Size = new System.Drawing.Size(1472, 762);
             this.panel2.TabIndex = 2;
             // 
             // txtResult
             // 
             this.txtResult.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtResult.Location = new System.Drawing.Point(2, 2);
-            this.txtResult.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtResult.Location = new System.Drawing.Point(3, 3);
             this.txtResult.Multiline = true;
             this.txtResult.Name = "txtResult";
             this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtResult.Size = new System.Drawing.Size(979, 505);
+            this.txtResult.Size = new System.Drawing.Size(1466, 756);
             this.txtResult.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(1405, 581);
+            this.ClientSize = new System.Drawing.Size(2108, 872);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "SqlToModel";
             ((System.ComponentModel.ISupportInitialize)(this.txtTableName.Properties)).EndInit();
